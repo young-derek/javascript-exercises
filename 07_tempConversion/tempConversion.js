7f1c19fa89e90c4dc87d9c15735f16688db02d1e
young-derek <derekyoung88@gmail.com>
2022-07-09 00:36:56 -0400

    Solve problem 7 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * 5 / 9 * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return Math.round(((celsius * 9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
