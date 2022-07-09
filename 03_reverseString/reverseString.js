f64fbf1c4fb32af3045a41e0ea9ac7195734057e
young-derek <derekyoung88@gmail.com>
2022-07-08 22:14:03 -0400

    Solve problem 3 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversedString = "";
+    for(let i = input.length; i > 0; i--)
+    {
+        reversedString += input[i - 1];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
