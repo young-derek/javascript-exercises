8a8cd2be9a4506a3293a8eaaa54d73e0920c5924
young-derek <derekyoung88@gmail.com>
2022-07-08 23:12:34 -0400

    Solve problem 5 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
 
+    if (typeof num1 != "number"
+        || typeof num2 != "number"
+        || num1 < 0
+        || num2 < 0) {
+        return "ERROR";
+    }
+
+    let smallNumber = Math.min(num1, num2);
+    let bigNumber = Math.max(num1, num2);
+    let total = 0;
+
+    for (; smallNumber <= bigNumber; smallNumber++) {
+        total += smallNumber;
+    }
+    return total;
 };
 
 // Do not edit below this line
