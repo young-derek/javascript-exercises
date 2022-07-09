79b0342805478e39aa06acec9f59f2d8bcab3344
young-derek <derekyoung88@gmail.com>
2022-07-08 22:59:03 -0400

    Solve problem 4 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    for (let i = 0; i < inputArray.length; i++)
+    {
+        if (args.includes(inputArray[i]))
+        {
+            inputArray.splice(i, 1);
+        }
+    }
 
+    return inputArray;
 };
 
 // Do not edit below this line
