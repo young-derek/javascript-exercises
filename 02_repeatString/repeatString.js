1524804a383e2eb1b9bb24486b7dbd5c1fe2909f
young-derek <derekyoung88@gmail.com>
2022-07-08 22:07:17 -0400

    Solve problem 2 - repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    if (repeat < 0)
+    {
+        return "ERROR";
+    }
+    
+    let finalString = "";
+    for (let i = 0; i < repeat; i++)
+    {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
