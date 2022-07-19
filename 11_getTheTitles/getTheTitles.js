66bd24d7c0c555d8aad63b2c56f08a658ceffdde
Derek <derekyoung_8@msn.com>
2022-07-19 14:19:02 -0400

    Solve problem 11 - getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titleArray = [];
+    for (let book of books)
+    {
+        titleArray.push(book.title);
+    }
+    return titleArray;
 };
 
 // Do not edit below this line
