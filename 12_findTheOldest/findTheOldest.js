8a3222850846a0929619a4edda1e716404013915
Derek <derekyoung_8@msn.com>
2022-07-19 15:38:44 -0400

    Solve problem 12 - findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+    let currentYear = new Date().getFullYear();
+    let oldestPerson = {};
+    let i = 0;
 
+    peopleArray.reduce(function (previousValue, currentValue) {
+        if (typeof currentValue.yearOfDeath === "undefined") {
+            if (currentYear - currentValue.yearOfBirth > previousValue) {
+                oldestPerson = peopleArray[i];
+                i++;
+                return currentYear - currentValue.yearOfBirth;
+            }
+        }
+        else {
+            if (currentValue.yearOfDeath - currentValue.yearOfBirth > previousValue) {
+                oldestPerson = peopleArray[i];
+                i++;
+                return currentValue.yearOfDeath - currentValue.yearOfBirth;
+            }
+        }
+    }, 0);
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
