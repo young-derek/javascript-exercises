dc5a71303559ef7e6fd598c81d7e90cc0bf1619d
Derek <derekyoung_8@msn.com>
2022-07-19 13:13:52 -0400

    Solve problem 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+    let currentNumber = 1;
+    let num1 = 0;
+    let num2 = 0;
+    let numberArray = [];
 
+    if (input >= 0) {
+        while (numberArray.length < input) {
+            numberArray.push(currentNumber);
+            num2 = num1;
+            num1 = currentNumber;
+            currentNumber = num1 + num2;
+        }
+    }
+    else {
+        return "OOPS";
+    }
+
+    return numberArray[input - 1];
 };
 
 // Do not edit below this line
