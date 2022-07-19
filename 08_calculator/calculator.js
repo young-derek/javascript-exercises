d9f1884a801d17c0f835569cf56587f4608ca017
Derek <derekyoung_8@msn.com>
2022-07-19 00:28:27 -0400

    Solve problem 8 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([...a]) {
+  total = 0;
+
+  for (i = 0; i < a.length; i++)
+  {
+    total += a[i];
+  }
+  return total;
+
 };
 
-const multiply = function() {
-
+const multiply = function([...a]) {
+  let total = 1
+  for (let i = 0; i < a.length; i++)
+  {
+    total *= a[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
+const factorial = function(a) {
 	
+  let total = 1;
+
+  for (let i = 0; i < a; i++)
+  {
+    total *= i+1;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
