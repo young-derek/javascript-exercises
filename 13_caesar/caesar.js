cf22145816041ab9de6ffdf1be321d8377607c00
Derek <derekyoung_8@msn.com>
2022-07-19 16:45:39 -0400

    Solve problem 13 partially, does not wrap

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,16 @@
-const caesar = function() {
+const caesar = function(inputString, inputNumber) {
+    let array = inputString.split('');
+    let regex = /[A-Za-z]/;
 
+    array.forEach((element, index) => {
+        if (regex.test(element))
+        {
+            array[index] = String.fromCharCode(element.charCodeAt(0) 
+                    + (inputNumber % 26));
+        }
+    });
+
+    return array.join('');
 };
 
 // Do not edit below this line
